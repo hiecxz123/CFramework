f7a1e9887b9200c5236db088fe5cb1701edfd1b0
hiecxz123 <2277520399@qq.com>
2023-09-12 17:03:23 +0800

    移除不必要的文件，添加多语言切换插件localization

--- Assets/CFramework/Scene/JsonLoad/GenerateCSharpJsonTest.cs
+++ Assets/CFramework/Scene/JsonLoad/GenerateCSharpJsonTest.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+public class TestData
+{
+    public int id;
+    public string name;
+    public DateTime dateTime;
 
+}
 
 public class GenerateCSharpJsonTest : MonoBehaviour
 {
@@ -12,13 +19,31 @@ public class GenerateCSharpJsonTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string json = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "TestCharacter.json"));
-        Debug.Log(json);
-        lists= LitJsonManager.JsonToObject<List<Sheet1>>(json);
-        Debug.Log(lists[2].Name);
-        Debug.Log(lists[1].Name);
-        Debug.Log(lists[0].Name);
-        Debug.Log(lists[2].HP);
+        //string json = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "TestCharacter.json"));
+        //Debug.Log(json);
+        //lists= LitJsonManager.JsonToObject<List<Sheet1>>(json);
+        //Debug.Log(lists[2].Name);
+        //Debug.Log(lists[1].Name);
+        //Debug.Log(lists[0].Name);
+        //Debug.Log(lists[2].HP);
+        //TestData testData = new TestData();
+        //testData.id = 1;
+        //testData.name = "skdljf";
+        //testData.dateTime = DateTime.Now;
+        //string json= LitJsonManager.ObjectToJson<TestData>(testData);
+        //File.WriteAllText(
+        //    Path.Combine(Application.streamingAssetsPath,
+        //    "TestData.json"),json
+        //    );
+
+        TestData testData1 = new TestData();
+
+        string jsonStr = File.ReadAllText(Path.Combine(Application.streamingAssetsPath,
+            "TestData.json"));
+
+        testData1 = LitJsonManager.JsonToObject<TestData>(jsonStr);
+
+        Debug.Log(testData1.dateTime);
 
     }
 
