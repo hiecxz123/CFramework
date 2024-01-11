237d7cbe83e9d075d2703418e448df30f5927c42
cxz <2277520399@qq.com>
2024-01-11 22:10:57 +0800

    添加事件中心

---
+++ Assets/CFramework/Event/EventCenter.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface IEventInfo
+{
+
+}
+
+public class EventInfo<T> : IEventInfo
+{
+    public UnityAction<T> actions;
+
+    public EventInfo(UnityAction<T> action)
+    {
+        actions += action;
+    }
+}
+
+public class EventInfo:IEventInfo
+{
+    public UnityAction actions;
+
+    public EventInfo(UnityAction action)
+    {
+        actions += action;
+    }
+}
+
+public class EventCenter : Singleton<EventCenter>
+{
+    //key —— 事件的名字（比如：怪物死亡，玩家死亡，通关 等等）
+    //value —— 对应的是 监听这个事件 对应的委托函数们
+    private Dictionary<EventEnum, IEventInfo> eventDic = new Dictionary<EventEnum, IEventInfo>();
+
+    /// <summary>
+    /// 添加事件监听
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="name"></param>
+    /// <param name="action"></param>
+    public void AddEventListener<T>(EventEnum name, UnityAction<T> action)
+    {
+        //有没有对应的事件监听
+        //有的情况
+        if (eventDic.ContainsKey(name))
+        {
+            (eventDic[name] as EventInfo<T>).actions += action;
+        }
+        //没有的情况
+        else
+        {
+            eventDic.Add(name, new EventInfo<T>(action));
+        }
+    }
+
+    /// <summary>
+    /// 监听不需要参数传递的事件
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="action"></param>
+    public void AddEventListener(EventEnum name, UnityAction action)
+    {
+        //有没有对应的事件监听
+        //有的情况
+        if (eventDic.ContainsKey(name))
+        {
+            (eventDic[name] as EventInfo).actions += action;
+        }
+        //没有的情况
+        else
+        {
+            eventDic.Add(name, new EventInfo(action));
+        }
+    }
+
+    /// <summary>
+    /// 移除对应的事件监听
+    /// </summary>
+    /// <param name="name">事件的名字</param>
+    /// <param name="action">对应之前添加的委托函数</param>
+    public void RemoveEventListener<T>(EventEnum name, UnityAction<T> action)
+    {
+        if (eventDic.ContainsKey(name))
+            (eventDic[name] as EventInfo<T>).actions -= action;
+    }
+
+    /// <summary>
+    /// 移除不需要参数的事件
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="action"></param>
+    public void RemoveEventListener(EventEnum name, UnityAction action)
+    {
+        if (eventDic.ContainsKey(name))
+            (eventDic[name] as EventInfo).actions -= action;
+    }
+
+    /// <summary>
+    /// 事件触发
+    /// </summary>
+    /// <param name="name">哪一个名字的事件触发了</param>
+    public void EventTrigger<T>(EventEnum name, T info)
+    {
+        //有没有对应的事件监听
+        //有的情况
+        if (eventDic.ContainsKey(name))
+        {
+            //eventDic[name]();
+            if ((eventDic[name] as EventInfo<T>).actions != null)
+                (eventDic[name] as EventInfo<T>).actions.Invoke(info);
+            //eventDic[name].Invoke(info);
+        }
+    }
+
+    /// <summary>
+    /// 事件触发（不需要参数的）
+    /// </summary>
+    /// <param name="name"></param>
+    public void EventTrigger(EventEnum name)
+    {
+        //有没有对应的事件监听
+        //有的情况
+        if (eventDic.ContainsKey(name))
+        {
+            //eventDic[name]();
+            if ((eventDic[name] as EventInfo).actions != null)
+                (eventDic[name] as EventInfo).actions.Invoke();
+            //eventDic[name].Invoke(info);
+        }
+    }
+
+    /// <summary>
+    /// 清空事件中心
+    /// 主要用在 场景切换时
+    /// </summary>
+    public void Clear()
+    {
+        eventDic.Clear();
+    }
+}
